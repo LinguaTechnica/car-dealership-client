4a2034d9bbed63283f30a51d13e3aba27df47319
Kay <kay.hudson@galvanize.com>
2021-09-15 17:06:02 -0700

    Add new component stubs.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { VehiclesService } from './vehicles.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'carDealer-client';
+  title = 'Auto Galaxy';
+  vehicles = [];
+
+  constructor(private vehicleService: VehiclesService) {
+    this.vehicleService.getAll().subscribe(vehicles => this.vehicles = vehicles);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { VehicleListComponent } from './vehicle-list/vehicle-list.component';
+import { VehicleDetailComponent } from './vehicle-detail/vehicle-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VehicleListComponent,
+    VehicleDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
