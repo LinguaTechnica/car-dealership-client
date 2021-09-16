60e0bc3fee4ca4f82ce62eb21d813c4d1441a9aa
Kay <kay.hudson@galvanize.com>
2021-09-15 20:31:27 -0700

    Add feature to search vehicles by keyword.

--- src/app/vehicle-search/vehicle-search.component.spec.ts
+++ src/app/vehicle-search/vehicle-search.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { VehicleSearchComponent } from './vehicle-search.component';
+import {FormBuilder, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {By} from '@angular/platform-browser';
 
 describe('VehicleSearchComponent', () => {
   let component: VehicleSearchComponent;
@@ -8,7 +10,8 @@ describe('VehicleSearchComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ VehicleSearchComponent ]
+      declarations: [ VehicleSearchComponent ],
+      imports: [ FormsModule, ReactiveFormsModule ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(VehicleSearchComponent);
@@ -21,7 +24,42 @@ describe('VehicleSearchComponent', () => {
   });
 
   it('should render vehicle search form', () => {
-    const searchEl = fixture.nativeElement.querySelector('form.vehicleSearch');
+    const searchEl = fixture.nativeElement.querySelector('.vehicleSearch form');
     expect(searchEl).not.toBeNull();
   });
+
+  it('should render search by keyword input field', () => {
+    const queryInput = fixture.nativeElement.querySelector('#searchQuery');
+
+    expect(queryInput).not.toBeNull();
+    expect(component.searchForm.controls.query).toBeTruthy();
+  });
+
+  it('should validate search form', () => {
+    const { query } = component.searchForm.controls;
+    expect(component.searchForm.valid).toBeFalse();
+    query.setValue('VW');
+    expect(component.searchForm.valid).toBeTrue();
+  });
+
+  it('should alert message for invalid form input < 2 characters', () => {
+    component.search();
+    fixture.detectChanges();
+    const alertEl = fixture.nativeElement.querySelector('#queryAlert');
+    expect(alertEl.textContent).toContain(component.getAlert());
+  });
+
+  it('should handle successful search query', () => {
+    const { query } = component.searchForm.controls;
+    query.setValue('VW');
+    component.search();
+    fixture.detectChanges();
+    const alertEl = fixture.nativeElement.querySelector('#queryAlert');
+    expect(alertEl).toBeNull();
+
+    component.searchEvent.subscribe(results => {
+      expect(results).toEqual([]);
+    });
+  });
+
 });
--- src/app/vehicle-search/vehicle-search.component.ts
+++ src/app/vehicle-search/vehicle-search.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-vehicle-search',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vehicle-search.component.css']
 })
 export class VehicleSearchComponent implements OnInit {
+  public searchForm: FormGroup;
+  @Output() public searchEvent: EventEmitter<[]> = new EventEmitter();
 
-  constructor() { }
+  private alertMessage: string | null;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.searchForm = this.formBuilder.group({
+      query: ['', [ Validators.required, Validators.minLength(2) ]]
+    });
+  }
+
+  search(): void {
+    if (!this.searchForm.valid) {
+      this.alertMessage = 'Enter a search query!';
+    } else {
+      this.searchEvent.emit([]);
+    }
+  }
+
+  getAlert(): string {
+    return this.alertMessage;
   }
 
 }
